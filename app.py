f5308b70cb50a16d3f34ecebffb230972c725d6a
Junior <iamjunioru@gmail.com>
2023-09-06 01:38:12 -0300

    feat: top 10 most popular games (by year)

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ df = pd.read_csv("games.csv")
 # data_url = "csvurl"
 # df = pd.read_csv(data_url)
 
-menu = st.sidebar.radio("navigation menu", ["home", "top 10 most popular (game genre)", "content 2"])
+menu = st.sidebar.radio("navigation menu:", ["home sweet home", "top 10 most popular (game genre)", "top 10 most popular game (by year)", "content 3"])
 
 def home():
     st.title("popular video games 1980 - 2023 🎮")
@@ -22,23 +22,31 @@ def game_genre():
     top_10_genero = df[df["Genres"] == genero_selecionado].nlargest(10, "Rating")
     
     fig, ax = plt.subplots()
-    ax.barh(top_10_genero["Title"], top_10_genero["Rating"])
+    ax.barh(top_10_genero["Title"], top_10_genero["Rating"],  color='black')
     ax.set_xlabel("rating")
     ax.set_ylabel("game(s)")
     ax.set_title(f"the 10 most popular {genero_selecionado}")
     
     st.pyplot(fig)
     
-def content2():
+def game_year_popular():
+    selected_year = st.slider("select year:", 1980, 2023, 2023)
+    year_data = df[df['Release Date'].str.contains(str(selected_year))]
+    year_data = year_data.sort_values(by='Times Listed', ascending=False).head(10)
+    
+    st.write(f"top 10 most popular game in {selected_year}:")
+    st.table(year_data[['Title', 'Times Listed']])
+
+def content3():
     st.write("esse marllo é bom mesmo")
     
 # menu select
-if menu == "home":
+if menu == "home sweet home":
     home()
 elif menu == "top 10 most popular (game genre)":
     game_genre()
-elif menu == "content 2":
-    content2()
+elif menu == "top 10 most popular game (by year)":
+    game_year_popular()
   
     
 # footer
