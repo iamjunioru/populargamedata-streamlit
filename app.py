ef8d0fd21804cc7628bc131141b6ec2f1ef31b85
Junior <iamjunioru@gmail.com>
2023-09-06 15:06:24 -0300

    feat/style: best rated games added and formmat style

--- app.py
+++ app.py
@@ -9,7 +9,15 @@ df = pd.read_csv("games.csv")
 # data_url = "csvurl"
 # df = pd.read_csv(data_url)
 
-menu = st.sidebar.radio("navigation menu:", ["home sweet home", "top 10 most popular game (by genre)", "top 10 most popular game (by year)", "top 10 most popular game (by dev)", "content 4"])
+menu = st.sidebar.radio("navigation menu:",
+                        ["home sweet home",
+                         "top 10 most popular game (by genre)",
+                         "top 10 most popular game (by year)",
+                         "top 10 most popular game (by dev)",
+                         "the 10 best rated games",
+                         "content 5",
+                         "content 6",
+                         "content 7"])
 
 def home():
     st.title("popular video games 1980 - 2023 🎮")
@@ -67,8 +75,22 @@ def games_by_developer():
     
     st.table(top_10_developer_games[["Title", "Rating"]])
     
-def content4():
-    st.write("esse marllo é bom mesmo")
+def best_rated():
+    st.subheader("the 10 best rated games")
+    
+    top_10_avaliados = df.nlargest(10, "Rating")
+    
+    st.table(top_10_avaliados[["Title", "Rating"]])
+    
+def content5():
+    st.write("content 5")
+    
+def content6():
+    st.write("content 6")
+
+def content7():
+    st.write("content 7")
+
     
 # menu select
 if menu == "home sweet home":
@@ -79,8 +101,14 @@ elif menu == "top 10 most popular game (by year)":
     game_year_popular()
 elif menu == "top 10 most popular game (by dev)":
     games_by_developer()
-elif menu == "content 4":
-    content4()
+elif menu == "the 10 best rated games":
+    best_rated()
+elif menu == "content 5":
+    content5()
+elif menu == "content 6":
+    content6()
+elif menu == "content 7":
+    content7()
   
     
 # footer
