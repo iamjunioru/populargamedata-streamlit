99d5f7ea01b96d7ae37853307c18770bf49507b2
Junior <iamjunioru@gmail.com>
2023-09-06 01:59:42 -0300

    chore: fixandadd search bar to best search

--- app.py
+++ app.py
@@ -9,23 +9,34 @@ df = pd.read_csv("games.csv")
 # data_url = "csvurl"
 # df = pd.read_csv(data_url)
 
-menu = st.sidebar.radio("navigation menu:", ["home sweet home", "top 10 most popular (game genre)", "top 10 most popular game (by year)", "content 3"])
+menu = st.sidebar.radio("navigation menu:", ["home sweet home", "top 10 most popular game (by genre)", "top 10 most popular game (by year)", "content 3"])
 
 def home():
     st.title("popular video games 1980 - 2023 🎮")
     st.dataframe(df)
 
 def game_genre():
-    st.subheader("top 10 most popular (game genre)")
+    st.subheader("top 10 most popular games (by genre)")
+    search_genre = st.text_input("search by gender:", key="genre_input")
 
-    genero_selecionado = st.selectbox("game genre:", df["Genres"].unique())
+    #  list genre sugest
+    generos_sugeridos = df["Genres"].unique()
+    
+    # filter
+    sugestoes_filtradas = [genero for genero in generos_sugeridos if search_genre.lower() in genero.lower()]
+
+    if not sugestoes_filtradas:
+        sugestoes_filtradas = generos_sugeridos
+
+    genero_selecionado = st.selectbox("select gender:", sugestoes_filtradas)
+
     top_10_genero = df[df["Genres"] == genero_selecionado].nlargest(10, "Rating")
     
     fig, ax = plt.subplots()
     ax.barh(top_10_genero["Title"], top_10_genero["Rating"],  color='black')
     ax.set_xlabel("rating")
     ax.set_ylabel("game(s)")
-    ax.set_title(f"the 10 most popular {genero_selecionado}")
+    ax.set_title(f"top 10 most popular games by genre {genero_selecionado}")
     
     st.pyplot(fig)
     
@@ -43,7 +54,7 @@ def content3():
 # menu select
 if menu == "home sweet home":
     home()
-elif menu == "top 10 most popular (game genre)":
+elif menu == "top 10 most popular game (by genre)":
     game_genre()
 elif menu == "top 10 most popular game (by year)":
     game_year_popular()
